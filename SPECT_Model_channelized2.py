5dd2816f5407b07449f9926d937fb56c71af0062
Scott Metzler <metzler@upenn.edu>
2024-05-23 15:41:13 -0500

    merging

--- SPECT_Model_channelized2.py
+++ SPECT_Model_channelized2.py
@@ -335,7 +335,7 @@ class SPECT_Model_channelized2(nn.Module):
         checkpoint = torch.load(filename) 
         self.load_state_dict(checkpoint['state_dict'])
 
-    def load_and_replace(self, filename, device):  #, device):
+    def load_and_replace(self, filename, device): 
         """
         This loads the model, checks model-dependent variables, and then fills the model
         """
